ce2a9a078ba088cb5ba0ac9cc6fe89b04386a897
yungshun317 <seraph317@gmail.com>
2023-08-15 09:19:41 +0800

    [FEAT] Use `CameraControls`

--- src/components/CameraControl.js
+++ src/components/CameraControl.js
@@ -1,6 +1,7 @@
-import {Grid} from "@react-three/drei";
+import {CameraControls, Grid} from "@react-three/drei";
 import {useRef} from "react";
 import * as THREE from "three/src/math/MathUtils";
+import {button, buttonGroup, useControls} from "leva";
 
 const CameraControl = () => {
     const cameraControlRef = useRef();
@@ -8,8 +9,46 @@ const CameraControl = () => {
     const { DEG2RAD } = THREE.MathUtils;
     console.log(DEG2RAD);
 
+    const cameraControls = useControls("Camera Controls", {
+        horizontalRotation: buttonGroup({
+            label: "Horizontal R",
+            opts: {
+                "45deg": () => cameraControlRef.current.rotate(45 * DEG2RAD, 0, true),
+                "-90deg": () => cameraControlRef.current.rotate(-90 * DEG2RAD, 0, true),
+                "360deg": () => cameraControlRef.current.rotate(360 * DEG2RAD, 0, true),
+            },
+        }),
+        verticalRotation: buttonGroup({
+            label: "Vertical R",
+            opts: {
+                "20deg": () => cameraControlRef.current.rotate(0, 20 * DEG2RAD, true),
+                "-40deg": () => cameraControlRef.current.rotate(0, -40 * DEG2RAD, true),
+            },
+        }),
+        truckGroup: buttonGroup({
+            label: "truck R",
+            opts: {
+                "(1,0)": () => cameraControlRef.current.truck(1, 0, true),
+                "(0,+1)": () => cameraControlRef.current.truck(0, 1, true),
+                "(-1,-1)": () => cameraControlRef.current.truck(-1, -1, true),
+            },
+        }),
+        zoomGroup: buttonGroup({
+            label: "zoom",
+            opts: {
+                0.25: () => cameraControlRef.current.zoom(0.25, true),
+                "-0.25": () => cameraControlRef.current.zoom(-0.25, true),
+            },
+        }),
+        lookAtBox: button(() => {
+            cameraControlRef.current.setLookAt(0, 1, 3, 0, 0, 0, true);
+        }),
+    });
+
     return (
         <>
+            <CameraControls ref={cameraControlRef} smoothTime={0.25} />
+
             <Grid
                 args={[30, 30]}
                 cellSize={0.25}
