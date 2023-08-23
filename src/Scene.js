8ff373e113d04004a8a669eca271464089856e0f
yungshun317 <seraph317@gmail.com>
2023-08-23 09:31:33 +0800

    [FEAT] Use `PresentationControls` with spring-physics, polar zoom, and snap-back

--- src/Scene.js
+++ src/Scene.js
@@ -1,11 +1,13 @@
 import CameraControl from "./components/CameraControl";
 import OrbitControl from "./components/OrbitControl";
+import PresentationControl from "./components/PresentationControl";
 
 const Scene = () => {
     return (
         <>
             { /* <CameraControl /> */ }
-            <OrbitControl />
+            { /* <OrbitControl /> */ }
+            <PresentationControl />
         </>
     );
 };
