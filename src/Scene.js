99a44e4c8ad3d5b4148e2bc9e12fdc0d79dcfb32
yungshun317 <seraph317@gmail.com>
2023-08-23 09:35:55 +0800

    [FEAT] Wrap an object with `TransformControls` to receive a transform gizmo

--- src/Scene.js
+++ src/Scene.js
@@ -1,13 +1,15 @@
 import CameraControl from "./components/CameraControl";
 import OrbitControl from "./components/OrbitControl";
 import PresentationControl from "./components/PresentationControl";
+import TransformControl from "./components/TransformControl";
 
 const Scene = () => {
     return (
         <>
             { /* <CameraControl /> */ }
             { /* <OrbitControl /> */ }
-            <PresentationControl />
+            { /* <PresentationControl /> */ }
+            <TransformControl />
         </>
     );
 };
